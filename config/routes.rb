8007b4a2540094f4274cae92a106b9bde238b57d
David Ader <davidjader@gmail.com>
2014-02-22 16:42:09 -0500

    Added devise views and pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Tammany::Application.routes.draw do
+  get "pages/home"
+  resources :page
+  
+
   devise_for :caseworkers
 
   # The priority is based upon order of creation:
@@ -50,7 +54,7 @@ Tammany::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'page#home'
 
   # See how all your routes lay out with "rake routes"
 
